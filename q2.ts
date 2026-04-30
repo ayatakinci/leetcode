c2b121e3643b04912a11c01a8959e389ad190911
ayatakinci <ayatalzidi2000@gmail.com>
2026-04-30 13:46:51 +0300

    q1 answer

---
+++ q2.ts
@@ -0,0 +1,11 @@
+/*
+Q2. What is a variable, really?
+Write a function that takes a name and returns a greeting. Then call it 3 times with different names.
+*/
+
+function hello (name:string){
+return "greeting"+" "+ name
+}
+console.log(hello('ayat'))
+console.log(hello("burak"))
+console.log(hello("dudu"))
