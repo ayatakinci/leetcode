6a3a07fc7cd02d7db3f5eb21f4541f046e0a886e
ayatakinci <ayatalzidi2000@gmail.com>
2026-04-30 14:14:23 +0300

    adding the q3 solution

--- q3.js
+++ q3.js
@@ -1,10 +1,11 @@
+"use strict";
 /*
 Q3. Count characters in a string — without .length
 Given a string like "hello", count how many characters it has by going through it one by one.
 */
-    let arr="hello"
-    let counter=0
-    for(let i=0; arr[i] !== undefined ; i++){
-        counter=counter+1
-        console.log(counter)
-    }
+let arr = "hello";
+let counter = 0;
+for (let i = 0; arr[i] !== undefined; i++) {
+    counter = counter + 1;
+    console.log(counter);
+}
