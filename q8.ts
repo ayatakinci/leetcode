fb6a8bb8b48336e20ce8f3b1c77a2a219cea4917
ayatakinci <ayatalzidi2000@gmail.com>
2026-05-07 11:41:42 +0300

    aumm all numbers in an array

---
+++ q8.ts
@@ -0,0 +1,11 @@
+/*
+ Sum all numbers in an array — without .reduce()
+Given [1, 2, 3, 4, 5], return
+*/
+let numbers2 = [1, 2, 3, 4, 5]
+let total=0
+for(let i=0; i< numbers2.length; i++){
+    total= total+numbers2[i]
+
+}
+console.log(total)
