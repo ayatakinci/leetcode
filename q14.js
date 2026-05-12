f99a89464e478c056d34207ee06cace2fc8458e8
ayatakinci <ayatalzidi2000@gmail.com>
2026-05-12 14:16:40 +0300

    Capitalize the first letter of every word in a sentence

---
+++ q14.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+Capitalize the first letter of every word in a sentence
+Given "hello world from typescript", return "Hello World From Typescript"
+*/
+let lowerCase = "hello world from typescript"; //. A string already lets you access each character by index, just like an array
+let upperCase = "";
+for (let i = 0; i < lowerCase.length; i++) {
+    if (i === 0) {
+        upperCase = lowerCase[i].toUpperCase();
+    }
+    else if (lowerCase[i - 1] === " ") { // If lowerCase[i-1] is a space → mena i the current character is the first letter of a new word → capitalize it
+        upperCase = upperCase + lowerCase[i].toUpperCase();
+    }
+    else { //Otherwise → just add it as is
+        upperCase = upperCase + lowerCase[i];
+    }
+}
+console.log(upperCase);
