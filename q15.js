3b80841d5cae08780a9c23c888c2318cee0edb3b
ayatakinci <ayatalzidi2000@gmail.com>
2026-05-14 14:29:06 +0300

    Find the most frequent element in an array

---
+++ q15.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ Find the most frequent element in an array
+Given [1, 2, 2, 3, 3, 3, 4], return 3.
+*/
+let numbersArray = [1, 2, 2, 3, 3, 3, 4];
+let mostFrequent = 0;
+let whichNumber = 0;
+let counts = {};
+for (let i = 0; i < numbersArray.length; i++) {
+    if (counts[numbersArray[i]] === undefined) {
+        counts[numbersArray[i]] = 1;
+    }
+    else {
+        counts[numbersArray[i]] = counts[numbersArray[i]] + 1;
+    }
+}
+for (let key in counts) {
+    if (mostFrequent < counts[key]) {
+        mostFrequent = counts[key];
+        whichNumber = Number(key);
+    }
+}
+console.log(mostFrequent);
+console.log(whichNumber);
--- q15.ts
+++ q15.ts
@@ -2,3 +2,28 @@
  Find the most frequent element in an array
 Given [1, 2, 2, 3, 3, 3, 4], return 3.
 */
+let numbersArray:number[] = [1, 2, 2, 3, 3, 3, 4];
+let mostFrequent:number = 0
+let whichNumber:number =0
+
+let counts: {
+    [key: number]:number
+}={}
+
+for(let i=0; i<numbersArray.length; i++){
+    if(counts[numbersArray[i]]=== undefined){
+        counts[numbersArray[i]] = 1
+    }else{
+        counts[numbersArray[i]] = counts[numbersArray[i]] + 1
+    }
+}
+
+ 
+for(let key in counts){
+    if(mostFrequent < counts[key]){
+    mostFrequent= counts[key]
+    whichNumber= Number(key)
+}
+
+}console.log(mostFrequent)
+console.log(whichNumber)
